646ebd300dfbaa1b1d0f2981e1b92b5e0f430fbd
Irina Shebarshina <iv.shebarshina@gmail.com>
2021-07-29 19:13:17 +0300

    Done task10_2

--- task10/task10_2.sql
+++ task10/task10_2.sql
@@ -59,8 +59,18 @@ FROM profiles p INNER JOIN communities_users cu ON (
 )
 ORDER BY cu.community_id ASC;
 
+-- самый молодой и самый старший пользователь в группе;
+SELECT * FROM 
+(SELECT cu.community_id, FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday ASC) AS oldest, 
+FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday DESC) AS youngest  
+FROM profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id )) AS T
+GROUP BY community_id;
+
 -- общее количество пользователей в группе;
-SELECT 
+SELECT COUNT(1)
 FROM communities_users cu 
 GROUP BY community_id ;
 
@@ -74,16 +84,64 @@ SELECT COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100
 FROM communities_users cu 
 GROUP BY community_id ;
 
--- Результирующий запрос
+-- запрос без самого молодого/старшего пользователя
 
 SELECT  community_id,
 AVG(COUNT(1)) OVER() AS avg_in_communities, -- среднее количество пользователей в группах;
 COUNT(1) AS total_in_community,  -- общее количество пользователей в группе;
-COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100 AS percentage -- отношение в процентах 
+COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100 AS percentage, -- отношение в процентах 
+(SELECT SUM(COUNT(1)) OVER() FROM profiles p ) AS total_users -- всего пользователей в системе;
 FROM communities_users cu INNER JOIN profiles p ON (p.user_id = cu.user_id)
 GROUP BY community_id;
 
+-- запрос без имени группы
 
+SELECT community_id, avg_in_communities, total_in_community, percentage, youngest, oldest
+FROM (
+(SELECT  community_id, 
+AVG(COUNT(1)) OVER() AS avg_in_communities, -- среднее количество пользователей в группах;
+COUNT(1) AS total_in_community,  -- общее количество пользователей в группе;
+COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100 AS percentage, -- отношение в процентах 
+(SELECT SUM(COUNT(1)) OVER() FROM profiles p ) AS total_users -- всего пользователей в системе;
+FROM communities_users cu INNER JOIN profiles p ON (p.user_id = cu.user_id)
+GROUP BY community_id) AS part1
+INNER JOIN 
+(SELECT DISTINCT cu.community_id AS si, FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday DESC) AS youngest,
+FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday ASC) AS oldest
+FROM profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id 
+)
+ORDER BY cu.community_id ASC) AS part2
+ON (part1.community_id = part2.si));
 
+-- имя группы
 
+SELECT DISTINCT cu.community_id, c.name AS group_name FROM (profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id 
+) INNER JOIN communities c ON (c.id = cu.community_id));
 
+-- Результирующий запрос
+
+SELECT community_id, community_name, avg_in_communities, total_in_community, percentage, youngest, oldest
+FROM (
+(SELECT  cu.community_id, co.name AS community_name,
+AVG(COUNT(1)) OVER() AS avg_in_communities, -- среднее количество пользователей в группах;
+COUNT(1) AS total_in_community,  -- общее количество пользователей в группе;
+COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100 AS percentage, -- отношение в процентах 
+(SELECT SUM(COUNT(1)) OVER() FROM profiles p ) AS total_users -- всего пользователей в системе;
+FROM (profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id 
+) INNER JOIN communities co ON (co.id = cu.community_id))
+GROUP BY community_id) AS part1
+INNER JOIN 
+(SELECT DISTINCT cu.community_id AS si, FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday DESC) AS youngest,
+FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday ASC) AS oldest
+FROM profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id 
+)
+ORDER BY cu.community_id ASC) AS part2
+ON (part1.community_id = part2.si));
