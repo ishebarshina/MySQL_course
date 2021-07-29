c7bcc68a6dab3a35237b5d7ae9d33c21d3c58a4f
Irina Shebarshina <iv.shebarshina@gmail.com>
2021-07-29 18:35:29 +0300

    Part of the resulting query task10_2

--- task10/task10_2.sql
+++ task10/task10_2.sql
@@ -38,13 +38,20 @@ ORDER BY cu.community_id ASC, p.birthday ASC ;
 
 -- самый молодой пользователь в группе;
 SELECT DISTINCT cu.community_id, FIRST_VALUE(p.user_id) 
-OVER(PARTITION BY cu.community_id ORDER BY p.birthday DESC)
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday DESC) AS youngest
 FROM profiles p INNER JOIN communities_users cu ON (
 	cu.user_id = p.user_id 
 )
 ORDER BY cu.community_id ASC;
 
 -- самый старший пользователь в группе;
+SELECT * FROM 
+(SELECT cu.community_id, FIRST_VALUE(p.user_id) 
+OVER(PARTITION BY cu.community_id ORDER BY p.birthday ASC) AS oldest  
+FROM profiles p INNER JOIN communities_users cu ON (
+	cu.user_id = p.user_id )) AS T
+GROUP BY community_id;
+
 SELECT DISTINCT cu.community_id, FIRST_VALUE(p.user_id) 
 OVER(PARTITION BY cu.community_id ORDER BY p.birthday ASC)
 FROM profiles p INNER JOIN communities_users cu ON (
@@ -53,19 +60,28 @@ FROM profiles p INNER JOIN communities_users cu ON (
 ORDER BY cu.community_id ASC;
 
 -- общее количество пользователей в группе;
-SELECT COUNT(1) 
+SELECT 
 FROM communities_users cu 
 GROUP BY community_id ;
 
 -- всего пользователей в системе;
 SELECT SUM(COUNT(1)) OVER()
-FROM users u
-GROUP BY u.id;
+FROM profiles p 
+GROUP BY p.user_id ;
 
 -- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100.
-SELECT COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM users u) * 100
+SELECT COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100
 FROM communities_users cu 
 GROUP BY community_id ;
+
+-- Результирующий запрос
+
+SELECT  community_id,
+AVG(COUNT(1)) OVER() AS avg_in_communities, -- среднее количество пользователей в группах;
+COUNT(1) AS total_in_community,  -- общее количество пользователей в группе;
+COUNT(1) / (SELECT SUM(COUNT(1)) OVER() FROM profiles p ) * 100 AS percentage -- отношение в процентах 
+FROM communities_users cu INNER JOIN profiles p ON (p.user_id = cu.user_id)
+GROUP BY community_id;
 
 
 
